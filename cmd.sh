3cd9022eca7e18c5ae31e3f3915c5fa84beb3f87
Zachary Doll <dollzachary@gmail.com>
2025-03-06 17:21:32 +0000

    updated new commands.

--- cmd.sh
+++ cmd.sh
@@ -21,3 +21,20 @@ runai submit eval \
     --large-shm \
     --preemptible \
     -- /bin/bash /mydata/vocim/xiaoran/scripts/bird_identity_classification/eval.sh
+
+python train.py --train_json_data /mydata/vocim/zachary/data/cropped_merged_annotations.json \
+                --eval_json_data /mydata/vocim/zachary/data/cropped_merged_annotations.json \
+                --img_dir /mydata/vocim/zachary/data/cropped
+
+python train.py --train_json_data /mydata/vocim/zachary/data/cropped_merged_annotations.json \
+                --eval_json_data /mydata/vocim/zachary/data/cropped_merged_annotations.json \
+                --img_dir /mydata/vocim/zachary/data/cropped 2>&1 | tee training.log
+
+
+runai submit job1 -i zacharyydoll/gpu_image:latest \
+                  --gpu 0.3 \
+                  --node-type A100 \
+                  --cpu 8 \
+                  --memory-limit 25G \
+                  --interactive \
+                  --attach
