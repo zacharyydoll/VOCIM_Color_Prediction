2284e063a735d289d7fc13f345532edf679ea7c9
Zachary Doll <dollzachary@gmail.com>
2025-03-07 13:07:04 +0000

    Added JSON files to gitignore, modified split_by_video to read from JSON instead of CSV, modified dataloader to letterbox to 512x512.

--- dataloader.py
+++ dataloader.py
@@ -1,17 +1,27 @@
 from torch.utils.data import DataLoader
 from dataset import ImageDataset
 from sampler import ClassBalancedSampler
+from PIL import ImageOps
 from torchvision import transforms
 
-def get_train_dataloder(json_file, img_dir, batch_size):
-    transform = transforms.Compose([
-    transforms.Resize((224,224)),  # Randomly crop and resize the image
-    #transforms.RandomRotation(30),      # Randomly rotate the image by up to 30 degrees
+def letterbox(img, size=(512, 512), fill_color=(0, 0, 0)):
+    """
+    Resize the image to fit within the size, while preserving aspect ratio, then pad 
+    with specified color such that the final output is exactly `size`(512 for tiny_vit_21m_512)
+    """
+    return ImageOps.pad(img, size, method=Image.BICUBIC, color=fill_color)
+
+train_transform = transforms.Compose([
+    transforms.Lambda(lambda img: letterbox(img, size=(512, 512))),
     transforms.RandomHorizontalFlip(),
-    transforms.RandomVerticalFlip(),    # Randomly flip the image vertically
-    transforms.ToTensor(),              # Convert image to tensor
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize
-    ])
+    transforms.RandomVerticalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                         std=[0.229, 0.224, 0.225])
+])
+
+def get_train_dataloder(json_file, img_dir, batch_size):
+    transform = train_transform # using letteboxing to 512
     train_data = ImageDataset(data_path = json_file, img_dir = img_dir, transform=transform)
     sampler = ClassBalancedSampler(train_data.annotations).get_sampler()
     train_dataloader = DataLoader(train_data, sampler = sampler, batch_size=batch_size)
@@ -19,11 +29,10 @@ def get_train_dataloder(json_file, img_dir, batch_size):
 
 def get_eval_dataloder(json_file, img_dir, batch_size):
     transform = transforms.Compose([
-    transforms.Resize((224,224)),  # Randomly crop and resize the image
-    #transforms.RandomRotation(30),      # Randomly rotate the image by up to 30 degrees
-    #transforms.RandomVerticalFlip(),    # Randomly flip the image vertically
-    transforms.ToTensor(),              # Convert image to tensor
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize
+        transforms.Lambda(lambda img: letterbox(img, size=(512, 512))),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
     ])
     eval_data = ImageDataset(data_path = json_file, img_dir = img_dir, transform=transform)
     # sampler = ClassBalancedSampler(eval_data.labels).get_sampler()
--- split_by_video.py
+++ split_by_video.py
@@ -1,90 +1,49 @@
 import json
-import os
 import copy
 import random
 import math
 
-def summarize_data(data_path):
-    import glob
-    import pandas as pd
+def get_video_groups(data):
+    video_groups = {}
 
-    annotation_folders = glob.glob(os.path.join(data_path, "*BP*/labeled*/BP*"))
-    summary = dict()
-    for folder in annotation_folders:
-        df_filename = [os.path.join(data_path, folder, i) for i in os.listdir(folder) if i.endswith('.csv')]
-        if len(df_filename):
-            df_filename = df_filename[0]
-        else:
-            df_filename = None
-            print(f"{folder} has no annotations")
-                
-        img_filenames = [os.path.join(folder,i) for i in os.listdir(folder) if i.endswith('.png') or i.endswith('.jpg')]
-        if not len(img_filenames):
-            print(f"{folder} has no images" )
+    for idx, image in enumerate(data['images']):
+        video_name = image['file_name'].split('/')[-2]
+        if video_name not in video_groups:
+            video_groups[video_name] = []
+        video_groups[video_name].append(idx)
+    return video_groups
 
-        if df_filename:
-            df = pd.read_csv(df_filename, header=[0,1,2,3])
-            ncols = len(df.columns)-3
-            nbird = ncols/10
-            if nbird not in summary.keys():
-                summary[nbird]=[[], []]
-            
-            summary[nbird][0].append(folder)
-            summary[nbird][1].append(img_filenames)
-    return summary
-
-
-def split(indices, ratio):
-
-    assert len(ratio)==3, "set split ratio such as [0.7, 0.2, 0.1]" 
-    train_ratio, val_ratio, test_ratio = ratio
-    assert train_ratio<1 and val_ratio<1 and test_ratio<1 and (train_ratio+val_ratio+test_ratio)>0.9999, "ratios should be lower than 1 and sum up to 1"
-
+def split_list(items, ratio):
     random.seed(42)
+    items = items[:]  # makes copy
+    random.shuffle(items)
 
-    random.shuffle(indices)
-    len_indices = len(indices)
+    n = len(items)
+    train_n = int(n * ratio[0])
+    val_n = int(n * ratio[1])
 
-    if len_indices<3:
-        out = [[indices[i]] if i<len_indices else [] for i in range(3)]
-        return 
-    if len_indices==3:
-        return [[indices[i]] for i in range(3)]
+    train = items[:train_n]
+    val = items[train_n:train_n+val_n]
+    test = items[train_n+val_n:]
 
-    intervals = [math.floor(len_indices * train_ratio), math.ceil(len_indices * val_ratio)]
-    intervals = [int(i) for i in intervals]
-
-    train_indices = indices[:intervals[0]]
-    val_indices = indices[intervals[0]:intervals[0]+intervals[1]]
-    test_indices = indices[intervals[0]+intervals[1]:]
-
-    if not len(test_indices) and len(val_indices)>1:
-        test_indices = [val_indices[-1]]
-        val_indices = val_indices[:-1]
-    return [train_indices, val_indices, test_indices] 
+    return train, val, test
 
 def save_split(data, indices, output_name):
     split_data = copy.deepcopy(data)
     indices = sorted(indices)
-    
     split_data['annotations'] = [data['annotations'][i] for i in indices]
-    data_ids = [data['annotations'][i]['id'] for i in indices]
     split_data['images'] = []
-    # split_data['images'] = [data['images'][i] for i in data_ids] 
 
     new_img_id = 0
     cur_id = -1
-    for i, entry in enumerate(split_data['annotations']):        
-        data_id = entry['id']
+
+    for i, entry in enumerate(split_data['annotations']):
         img_id = entry['image_id']
-
-        if cur_id ==-1 or img_id!=cur_id:
+        if cur_id == -1 or img_id != cur_id:
             cur_id = img_id
-
             split_data['images'].append(data['images'][img_id])
             split_data['images'][-1]['id'] = new_img_id
-            new_img_id+=1
-
+            new_img_id += 1
         entry['id'] = i
         entry['image_id'] = split_data['images'][-1]['id']
 
@@ -93,106 +52,21 @@ def save_split(data, indices, output_name):
         print(f'data split saved to {output_name}')
 
 def main():
-    with open('/mydata/vocim/xiaoran/scripts/multiview_video_keypoint_predict/vocim_yolopose_trainvaltest.json', 'r') as f:
+    with open('/mydata/vocim/zachary/data/cropped_merged_annotations.json', 'r') as f:
         data = json.load(f)
-
-    test_videos = ['BP_2021-06-01_15-05-46_096578_0000000', 'BP_2022-09-07_12-33-47_959910_0000000', 'BP_2023-06-23_14-44-16_556681_0380000']
-    data_by_n = dict()
-
-    train_ids_all_n = []
-    val_ids_all_n = []
-    test_ids_all_n = []
-
-    train_video_list = []
-    val_video_list = []
-    test_video_list = []
-
-    data_by_n = dict()
-    summary = summarize_data('/mydata/vocim/shared/KeypointAnnotations')
-
-    for k in summary.keys():
-        for v in summary[k][0]:
-            _v = v.split('/')[-1]
-            data_by_n[_v] = k
     
-    for k in summary.keys():
-        data_summary = dict()
-        
-        for image, anno in zip(data['images'], data['annotations']):
-            video_name = image['file_name'].split('/')[-2]
-            if data_by_n[video_name]!=k or video_name in test_videos:
-                continue
+    video_groups = get_video_groups(data) # Get video grps directly from master json instead of CSV
+    video_names = list(video_groups.keys())
+    train_videos, val_videos, test_videos = split_list(video_names, [0.7, 0.2, 0.1])
 
-            idx = anno['id']
-            year = video_name[3:7]
+    # Collect annotations idx for each group 
+    train_ids = [idx for video in train_videos for idx in video_groups[video]]
+    val_ids = [idx for video in val_videos for idx in video_groups[video]]
+    test_ids = [idx for video in test_videos for idx in video_groups[video]]
 
-            if year not in data_summary.keys():
-                data_summary[year] = dict()
-            if video_name not in data_summary[year].keys():
-                data_summary[year][video_name] = []
-            
-            data_summary[year][video_name].append(idx)
+    save_split(data, train_ids, 'data/vocim_yolopose_train_vidsplit.json')
+    save_split(data, val_ids, 'data/vocim_yolopose_val_vidsplit.json')
+    save_split(data, test_ids, 'data/vocim_yolopose_test_vidsplit.json')
 
-        # for image, anno in zip(data['images'], data['annotations']):
-        #     video_name = image['file_name'].split('/')[-2]
-        #     if data_by_n[video_name]!=k or video_name in test_videos:
-        #         continue
-
-        #     color_label = anno['identity']
-        #     idx = anno['id']
-            
-        #     if color_label not in data_summary.keys():
-        #         data_summary[color_label] = dict()
-            
-        #     if video_name not in data_summary[color_label].keys():
-        #         data_summary[color_label][video_name] = []
-
-        #     data_summary[color_label][video_name].append(idx)
-
-        train_ids = []
-        val_ids = []
-        test_ids = []
-
-        for year in data_summary.keys():
-            vids = list(data_summary[year].keys())
-            vid_indices = list(range(len(vids)))
-            train, val, test = split(vid_indices, [0.7, 0.2, 0.1])
-            # import pdb
-            # pdb.set_trace()
-            train_ids.extend([data_summary[year][vids[i]] for i in train])
-            val_ids.extend([data_summary[year][vids[i]] for i in val])
-            test_ids.extend([data_summary[year][vids[i]] for i in test])
-
-            train_vids = [vids[i] for i in train]
-            val_vids = [vids[i] for i in val]
-            test_vids = [vids[i] for i in test]
-
-            train_video_list.extend(train_vids)
-            val_video_list.extend(val_vids)
-            test_video_list.extend(test_vids)
-
-        train_ids = [i for ids in train_ids for i in ids]
-        val_ids = [i for ids in val_ids for i in ids]
-        test_ids = [i for ids in test_ids for i in ids]
-
-        train_ids_all_n.append(train_ids)
-        val_ids_all_n.append(val_ids)
-        test_ids_all_n.append(test_ids)
-
-    video_split = {'train': train_video_list,
-                    'val': val_video_list,
-                    'test': test_video_list}
-
-    with open('video_split.json', 'w') as f:
-        json.dump(video_split, f)
-
-    train_ids_all_n = [i for ids in train_ids_all_n for i in ids]
-    val_ids_all_n = [i for ids in val_ids_all_n for i in ids]
-    test_ids_all_n = [i for ids in test_ids_all_n for i in ids]
-
-    save_split(data, train_ids_all_n, 'data/vocim_yolopose_train_vidsplit.json')
-    save_split(data, val_ids_all_n, 'data/vocim_yolopose_val_vidsplit.json')
-    save_split(data, test_ids_all_n, 'data/vocim_yolopose_test_vidsplit.json')
-
-
-main()
+if __name__ == "__main__":
+    main()
